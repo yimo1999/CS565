afc21859ec65053c2cbb6f0bd6924bdcb7ac0015
Mo <moyicong1999@163.com>
2023-04-01 12:42:46 -0500

    update test user_profile.csv

--- main.py
+++ main.py
@@ -2,4 +2,14 @@ from UserProfile import profile_collector
 from UserEvents import *
 
 if __name__ == '__main__':
-    event_logger('shhr3y')
+    profile_collector('shhr3y')
+    profile_collector('mrshenli')
+    profile_collector('shaoyf42')
+    profile_collector('ykddd')
+    profile_collector('udbhav1')
+    profile_collector('suo')
+    profile_collector('Valentine233')
+    profile_collector('z71258847')
+    profile_collector('huydhn')
+    profile_collector('H-Huang')
+    profile_collector('jerryzh168')
